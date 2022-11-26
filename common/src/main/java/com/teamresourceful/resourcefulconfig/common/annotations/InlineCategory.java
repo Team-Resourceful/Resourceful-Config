e6b00cbe5a63c66997bb811e0107a7898bbf4036
ThatGravyBoat <thatgravyboat@gmail.com>
2022-11-26 16:36:12 -0330

    Fixed InlineCategory having wrong type

--- common/src/main/java/com/teamresourceful/resourcefulconfig/common/annotations/InlineCategory.java
+++ common/src/main/java/com/teamresourceful/resourcefulconfig/common/annotations/InlineCategory.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-@Target({ElementType.TYPE})
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface InlineCategory {
 }

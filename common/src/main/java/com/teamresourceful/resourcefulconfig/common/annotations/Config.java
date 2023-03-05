4009cf180e04f5ad52135df7864f100af16953d6
ThatGravyBoat <thatgravyboat@gmail.com>
2023-03-05 15:43:26 -0330

    Forgot about sub dirs in config path pattern

--- common/src/main/java/com/teamresourceful/resourcefulconfig/common/annotations/Config.java
+++ common/src/main/java/com/teamresourceful/resourcefulconfig/common/annotations/Config.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Config {
 
-    @Pattern("^[a-z0-9_-]+$")
+    @Pattern("^[a-z0-9_/-]+$")
     String value();
 }

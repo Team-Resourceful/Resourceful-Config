03225c3b43a03dac071234c3bcbd4f4e030e1bb8
MayaqqDev <i@mayaqq.dev>
2024-10-05 16:56:15 +0200

    Removed hardcoded literals and added czech translation

--- common/src/main/java/com/teamresourceful/resourcefulconfig/client/UIConstants.java
+++ common/src/main/java/com/teamresourceful/resourcefulconfig/client/UIConstants.java
@@ -13,14 +13,14 @@ public class UIConstants {
     public static final int SPACING = 4;
 
     public static final Component BACK = CommonComponents.GUI_BACK;
-    public static final Component CLOSE = Component.literal("Close");
-    public static final Component RESET = Component.literal("Reset");
-    public static final Component EDIT = Component.literal("Edit");
-    public static final Component EDIT_STRING = Component.literal("Edit String");
-    public static final Component EDIT_LIST = Component.literal("Edit List");
-    public static final Component CHOOSE_ITEM = Component.literal("Choose Item: ");
-    public static final Component ADD_ITEM = Component.literal("Add Item");
-    public static final Component EDIT_OBJECT = Component.literal("Edit Object");
-    public static final Component MOD_CONFIGS = Component.literal("Mod Configs").withColor(UIConstants.TEXT_TITLE);
-    public static final Component MOD_CONFIGS_DESCRIPTION = Component.literal("Select a config to view or edit.").withColor(UIConstants.TEXT_PARAGRAPH);
+    public static final Component CLOSE = Component.translatable("rconfig.ui.constant.close");
+    public static final Component RESET = Component.translatable("rconfig.ui.constant.reset");
+    public static final Component EDIT = Component.translatable("rconfig.ui.constant.edit");
+    public static final Component EDIT_STRING = Component.translatable("rconfig.ui.constant.edit.string");
+    public static final Component EDIT_LIST = Component.translatable("rconfig.ui.constant.edit.list");
+    public static final Component EDIT_OBJECT = Component.translatable("rconfig.ui.constant.edit.object");
+    public static final Component CHOOSE_ITEM = Component.translatable("rconfig.ui.constant.choose_item");
+    public static final Component ADD_ITEM = Component.translatable("rconfig.ui.constant.add_item");
+    public static final Component MOD_CONFIGS = Component.translatable("rconfig.ui.constant.mod_configs").withColor(UIConstants.TEXT_TITLE);
+    public static final Component MOD_CONFIGS_DESCRIPTION = Component.translatable("rconfig.ui.constant.mod_configs.description").withColor(UIConstants.TEXT_PARAGRAPH);
 }
